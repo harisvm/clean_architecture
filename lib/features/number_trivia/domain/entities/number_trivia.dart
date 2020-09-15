f209baab7f01fbdb677dfc6b0c921c9da4f56fdf
Appeonix <hari.n@appeonix.com>
2020-09-15 16:57:10 +0530

    num_tri_repo

--- lib/features/number_trivia/domain/entities/number_trivia.dart
+++ lib/features/number_trivia/domain/entities/number_trivia.dart
@@ -1,7 +1,13 @@
 import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
 
-class NumberTrivia extends Equatable{
+class NumberTrivia extends Equatable {
+  final String text;
+  final String number;
 
+  NumberTrivia({@required this.text, @required this.number})
+      : super();
 
-
+  @override
+  List<Object> get props => [];
 }
