ace25cd0e7a89bdd4e42a51a475c51abed642a4b
Appeonix <hari.n@appeonix.com>
2020-09-15 17:25:11 +0530

    repository returns either success object or failure object

--- lib/features/number_trivia/data/repositories/number_trivia_repository.dart
+++ lib/features/number_trivia/data/repositories/number_trivia_repository.dart
@@ -1,1 +1,9 @@
-abstract class NumberTriviaRepository {}
+import 'package:clean_architecture_tdd/features/core/error/failures.dart';
+import 'package:clean_architecture_tdd/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class NumberTriviaRepository {
+ Future<Either<Failure,NumberTrivia>> getConcreteNumberTrivia(int number);
+
+ Future<Either<Failure,NumberTrivia>>  getRandomNumberTrivia();
+}
